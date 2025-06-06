b2df070e1d38b2a9c477f6efd9a4534ec9c2da80
amol <ap195569@gmail.com>
2025-06-07 02:02:52 +0530

    Update index.js to fix __dirname issue

--- back/index.js
+++ back/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import user from './model/user.js'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 
 
@@ -15,6 +16,12 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
 
